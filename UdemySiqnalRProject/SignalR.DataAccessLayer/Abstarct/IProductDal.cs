f96c828ca219ed9189f4f29434247129ca5df8e0
Samir Huseynli <samirhueynli2803@gmail.com>
2024-01-30 23:02:34 +0400

    Completed DataAccessLayer

---
+++ UdemySiqnalRProject/SignalR.DataAccessLayer/Abstarct/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using SiqnalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DataAccessLayer.Abstarct
+{
+    public interface IProductDal:IGenericDal<Product>
+    {
+    }
+}
