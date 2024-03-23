cf8f5b5c64c1d21925ec096bf2d8f8039300e929
Samir Huseynli <samirhueynli2803@gmail.com>
2024-03-24 03:48:30 +0400

    Commit 24.03.2024 03.48

---
+++ UdemySiqnalRProject/SiqnalRWebUI/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiqnalRWebUI.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult ChatList()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UdemySiqnalRProject/SiqnalRWebUI/ViewComponents/Message/_ChatListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiqnalRWebUI.ViewComponents.Message
+{
+    public class _ChatListComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
